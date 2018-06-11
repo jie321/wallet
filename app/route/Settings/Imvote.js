04590aed51e332ee6b8d1cc605f5ce5a408f9067
guy <guy@qq.com>
2018-06-11 17:50:58 +0800

    issue ##35

--- app/route/Settings/Imvote.js
+++ app/route/Settings/Imvote.js
@@ -9,8 +9,13 @@ import Icon from 'react-native-vector-icons/Ionicons'
 import UImage from '../../utils/Img'
 import { EasyLoading } from '../../components/Loading';
 import { EasyToast } from '../../components/Toast';
+import {EasyDialog} from '../../components/Dialog'
 
-@connect(({vote}) => ({...vote}))
+import { Eos } from "react-native-eosjs";
+var AES = require("crypto-js/aes");
+var CryptoJS = require("crypto-js");
+
+@connect(({wallet, vote}) => ({...wallet, ...vote}))
 class Imvote extends React.Component {
  
     static navigationOptions = ({ navigation }) => {
@@ -37,97 +42,118 @@ class Imvote extends React.Component {
             isShowBottom: false,
             selectMap: new Map(),
             // preIndex: 0 // 声明点击上一个按钮的索引  **** 单选逻辑 ****
+            votelist: [],
         };
     }
 
     componentDidMount() {
         this.props.dispatch({
             type: 'wallet/getDefaultWallet', callback: (data) => {
-                if(!data || !data.defaultWallet || !data.defaultWallet.account){
-                    EasyToast.show("请先创建钱包");
-                }else{
-                    this.props.dispatch({ type: 'vote/getvotinginfo', payload: { voter: data.defaultWallet.account} });
-                }
+                this.props.dispatch({ type: 'vote/list', payload: { page:1} });
+                this.props.dispatch({ type: 'vote/getaccountinfo', payload: { page:1,username: data.defaultWallet.account} });
             }
-        });
-
+        })
     }
 
-    deleteItem = () => { // 删除
-        let {selectMap} = this.state;
-        let valueArr = [...selectMap.values()];
-        let keyArr = [...selectMap.keys()];
-        // alert("确认撤票" + valueArr)
+    unapprove = (rowData) => { // 选中用户
         if(!this.props.defaultWallet){
             EasyToast.show('请先创建钱包');
             return;
         }
 
-        const view =
-        <View style={{ flexDirection: 'row' }}>
-            <TextInput autoFocus={true} onChangeText={(password) => this.setState({ password })} returnKeyType="go" selectionColor="#65CAFF"
-                secureTextEntry={true}
-                keyboardType="ascii-capable" style={{ color: '#65CAFF', marginLeft: 10, width: 120, height: 45, fontSize: 15, backgroundColor: '#EFEFEF' }}
-                placeholderTextColor="#8696B0" placeholder="请输入密码" underlineColorAndroid="transparent" />
-        </View>
-
-        EasyDialog.show("密码", view, "确认", "取消", () => {
-
-        if (this.state.password == "") {
-            EasyToast.show('请输入密码');
+        if(!this.props.producers || this.props.producers.length <=0){
+            EasyToast.show('您还未投票');
             return;
         }
-        EasyLoading.show();
 
-        var privateKey = this.props.defaultWallet.activePrivate;
-        try {
-            var bytes_privateKey = CryptoJS.AES.decrypt(privateKey, this.state.password + this.props.defaultWallet.salt);
-            var plaintext_privateKey = bytes_privateKey.toString(CryptoJS.enc.Utf8);
-            if (plaintext_privateKey.indexOf('eostoken') != -1) {
-                plaintext_privateKey = plaintext_privateKey.substr(8, plaintext_privateKey.length);
-                // alert("plaintext_privateKey "+plaintext_privateKey);
+        var selectArr= [];
+        for(var i = 0; i < this.props.producers.length; i++){
+            selectArr.push(this.props.producers[i].owner);
+        }
+        const { dispatch } = this.props;
+        // alert("this.props.voteData "+ JSON.stringify(this.props.voteData));
+        this.props.voteData.forEach(element => {
+            if(element.isChecked){
+                // selectArr.push(element.owner);
+                selectArr.splice(selectArr.indexOf(element.owner), 1);
+            }
+        });
+        
+        // var producersList = "";
+        // for(count=0;count<selectArr.length;count++){
+        //     producersList += " " + selectArr[count]
+        // } 
+        // alert("producersList: " + producersList + " "+ JSON.stringify(this.props.producers));
 
-                //投票
-                Eos.transaction({
-                    actions:[
-                        {
-                            account: 'eosio',
-                            name: 'voteproducer',
-                            authorization: [{
-                                actor: this.props.defaultWallet.account,
-                                permission: 'active'
-                            }],
-                            data:{
-                                voter: this.props.defaultWallet.account,
-                                proxy: '',
-                                producers: ["producer111j", "producer111p"]
+        // this.props.dispatch({ type: 'vote/addvote', payload: { keyArr: selectArr}});    
+        // alert("this.props.defaultWallet.account: " + this.props.defaultWallet.account);
+            const view =
+            <View style={{ flexDirection: 'column', alignItems: 'center', }}>
+                <TextInput autoFocus={true} onChangeText={(password) => this.setState({ password })} returnKeyType="go" selectionColor="#65CAFF"
+                    secureTextEntry={true}
+                    keyboardType="ascii-capable" style={{ color: '#65CAFF', height: 45, width: 160, paddingBottom: 5, fontSize: 16, backgroundColor: '#FFF', borderBottomColor: '#586888', borderBottomWidth: 1, }}
+                    placeholderTextColor="#8696B0" placeholder="请输入密码" underlineColorAndroid="transparent" />
+                <Text style={{ fontSize: 14, color: '#808080', lineHeight: 25, marginTop: 5,}}></Text>  
+            </View>
+    
+            EasyDialog.show("请输入密码", view, "确认", "取消", () => {
+    
+            if (this.state.password == "") {
+                EasyToast.show('请输入密码');
+                return;
+            }
+            EasyLoading.show();
+
+            var privateKey = this.props.defaultWallet.activePrivate;
+            try {
+                var bytes_privateKey = CryptoJS.AES.decrypt(privateKey, this.state.password + this.props.defaultWallet.salt);
+                var plaintext_privateKey = bytes_privateKey.toString(CryptoJS.enc.Utf8);
+                if (plaintext_privateKey.indexOf('eostoken') != -1) {
+                    plaintext_privateKey = plaintext_privateKey.substr(8, plaintext_privateKey.length);
+                    // alert("plaintext_privateKey "+plaintext_privateKey);
+
+                    //撤票
+                    Eos.transaction({
+                        actions:[
+                            {
+                                account: 'eosio',
+                                name: 'voteproducer',
+                                authorization: [{
+                                    actor: this.props.defaultWallet.account,
+                                    permission: 'active'
+                                }],
+                                data:{
+                                    voter: this.props.defaultWallet.account,
+                                    proxy: '',
+                                    producers:  selectArr, //["producer111f"]
+                                }
                             }
+                        ]
+                    }, plaintext_privateKey, (r) => {
+                        EasyLoading.dismis();
+                        // alert(JSON.stringify(r.data));
+                        if(r.data && r.data.transaction_id){
+                            this.props.dispatch({ type: 'vote/getaccountinfo', payload: { page:1,username: this.props.defaultWallet.account} });
+                            EasyToast.show("撤票成功");
+                        }else if(r.data && JSON.parse(r.data).code != 0){
+                            var jdata = JSON.parse(r.data);
+                            var errmsg = "撤票失败: ";
+                            if(jdata.error.details[0].message){
+                                errmsg = errmsg + jdata.error.details[0].message;
+                            }
+                            alert(errmsg);
                         }
-                    ]
-                }, plaintext_privateKey, (r) => {
+                    }); 
+                } else {
                     EasyLoading.dismis();
-                    // alert(JSON.stringify(r.data));
-                    if(r.data && r.data.transaction_id){
-                        EasyToast.show("撤票成功");
-                    }else if(r.data && JSON.parse(r.data).code != 0){
-                        var jdata = JSON.parse(r.data);
-                        var errmsg = "投撤票失败: ";
-                        if(jdata.error.details[0].message){
-                            errmsg = errmsg + jdata.error.details[0].message;
-                        }
-                        alert(errmsg);
-                    }
-                }); 
-            } else {
+                    EasyToast.show('密码错误');
+                }
+            } catch (e) {
                 EasyLoading.dismis();
                 EasyToast.show('密码错误');
             }
-        } catch (e) {
-            EasyLoading.dismis();
-            EasyToast.show('密码错误');
-        }
-        EasyDialog.dismis();
-    }, () => { EasyDialog.dismis() });
+            EasyDialog.dismis();
+        }, () => { EasyDialog.dismis() });
     };
 
 
@@ -152,7 +178,7 @@ class Imvote extends React.Component {
                 </View>
                 <ListView style={{flex:1,}} renderRow={this.renderRow} enableEmptySections={true} 
                
-                dataSource={this.state.dataSource.cloneWithRows(this.props.voteData == null ? [] : this.props.voteData)} 
+                dataSource={this.state.dataSource.cloneWithRows(this.props.producers == null ? [] : this.props.producers)} 
                 //dataSource={this.state.dataSource.cloneWithRows(list.data == null ? [] : JSON.parse(list.data).rows)} 
                 renderRow={(rowData, sectionID, rowID) => ( // cell样式                 
                     <View>
@@ -182,11 +208,11 @@ class Imvote extends React.Component {
                 <View style={styles.footer}>
                     <Button  style={{ flex: 1 }}>
                         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', flexDirection: 'column', marginRight: 1, backgroundColor: UColor.mainColor, }}>
-                            <Text style={{ fontSize: 18, color: '#F3F4F4' }}>23</Text>
+                            <Text style={{ fontSize: 18, color: '#F3F4F4' }}>{this.props.producers == null ? 30 : 30 - this.props.producers.length}</Text>
                             <Text style={{ fontSize: 14, color: '#8696B0' }}>剩余可投票数</Text>
                         </View>
                     </Button>
-                    <Button onPress={this.deleteItem.bind(this)} style={{ flex: 1 }}>
+                    <Button onPress={this.unapprove.bind(this)} style={{ flex: 1 }}>
                         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', flexDirection: 'row', marginLeft: 1, backgroundColor: UColor.mainColor, }}>
                             <Image source={UImage.vote_h} style={{ width: 30, height: 30 }} />
                             <Text style={{ marginLeft: 20, fontSize: 18, color: UColor.fontColor }}>撤票</Text>
