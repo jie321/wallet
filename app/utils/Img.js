e381ec2de199b845ad3ac38b6202d4c95c5f4884
bille18 <flying_why17@outlook.com>
2018-07-16 14:55:12 +0800

    分享图标统一，删除不用的图标

--- app/utils/Img.js
+++ app/utils/Img.js
@@ -88,7 +88,6 @@ export default {
   nash:require('../img/nash.png'),
   network:require('../img/network.png'),
   Invitation_vote:require('../img/Invitation_vote.png'),
-  turnin_share:require('../img/turninshare.png'),
   turnin_head:require('../img/turninhead.png'),
   transfer:require('../img/transfer.png'),
 }
