ae50c30e96acb6284e33a53c2dcbdb7c83b108a0
marco <lxgaisqq@163.com>
2018-07-16 12:34:12 +0800

    Fixed #243

--- app/models/Assets.js
+++ app/models/Assets.js
@@ -125,6 +125,11 @@ export default {
             var isBalanceChange = false;
             for(let i in myAssets){
                 let item = myAssets[i];
+                var accountName = yield call(store.get, 'accountName');
+                if(accountName == null || payload.accountName != accountName){ // 切换用户后
+                    isBalanceChange = true;
+                    item.balance = '0.0000';
+                }
                 const resp = yield call(Request.request, getBalance, 'post', {contract: item.asset.contractAccount, account: payload.accountName, symbol: item.asset.name});
                 // alert("------ " + JSON.stringify(resp));
                 if(resp && resp.code=='0' && resp.data != null && resp.data != ""){
@@ -139,6 +144,8 @@ export default {
                 // var myAssetsNew = yield call(store.get, 'myAssets');
                 // if(myAssetsNew != null && myAssetsNew.length == myAssets.length){
                     // alert("getBalance" +JSON.stringify(myAssets));
+
+                    yield call(store.save, 'accountName', payload.accountName);
                     yield call(store.save, 'myAssets', myAssets);
                     yield put({ type: 'updateMyAssets', payload: {myAssets: myAssets} });
                 // }
