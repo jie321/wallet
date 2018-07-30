c21a0a24d1e7eb34c9ccff77990836d3e9712bfa
marco <lxgaisqq@163.com>
2018-07-30 15:35:02 +0800

    v2.1.9.1 加上账号创建和激活流程网络异常检测防护

--- app/route/Settings/index.js
+++ app/route/Settings/index.js
@@ -186,8 +186,8 @@ class Setting extends React.Component {
           </View>
           <View style={styles.footer}>
             <Text style={styles.foottext}>© 2018 eostoken all rights reserved </Text>
-            <Text style={styles.foottext}>EOS专业版钱包 V{DeviceInfo.getVersion()}</Text>
-            {/* <Text style={styles.foottext}>EOS专业版钱包 V2.1.8</Text> */}
+            {/* <Text style={styles.foottext}>EOS专业版钱包 V{DeviceInfo.getVersion()}</Text> */}
+            <Text style={styles.foottext}>EOS专业版钱包 V2.1.9.1</Text>
           </View>
         </View>
       </ScrollView>
